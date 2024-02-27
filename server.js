5038cc161688adaeee9ec8ffcd0c96bc92159d2b
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-02-26 19:22:09 -0800

    simple routing

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const app = express();   // just a convention to use app as the name of the expr
 const path = require('path');
 const PORT = process.env.PORT || 3500;  // set the port to 3500
 
+// basic route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
