9da0ef3b8a37140e6623717055b2e53b1e664222
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-02-26 19:25:54 -0800

    use route to serve file

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();   // just a convention to use app as the name of the expr
 const path = require('path');
 const PORT = process.env.PORT || 3500;  // set the port to 3500
 
-// basic route
+// // basic route
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
+
+// route to serve the index.html file
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); // use path.join to make it easier to concatenate paths
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
